2302c1694d9ae0ded299ddfe5b34a6f49f0b40f8
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2024-09-19 10:34:51 -0500

    Update map.js

--- Website/scripts/map.js
+++ Website/scripts/map.js
@@ -17,7 +17,7 @@ function setBackground() {
     var context = canvas.getContext("2d");
     
 
-    context.setBackground('../images/CUW-map.png');
+    context.setBackground('https://simeonbauman.github.io/Capstone-24/Website/images/CUW-map.png');
 }
 
 function drawMap(){
