a3afbb66f8576c52c6651062b761992b1634b3d0
SimeonBauman <simeonbauman@outlook.com>
2024-10-05 18:18:12 -0500

    Update directions.js

--- Website/scripts/directions.js
+++ Website/scripts/directions.js
@@ -1,3 +1,10 @@
+// Function to compute the vector from two points
+function vectorFromPoints(p1, p2) {
+    return {
+        x: p2.x - p1.x,
+        y: p2.y - p1.y
+    };
+}
 // Function to compute the cross product of two vectors
 function crossProduct(v1, v2) {
     return v1.x * v2.y - v1.y * v2.x;
@@ -5,10 +12,8 @@ function crossProduct(v1, v2) {
 
 // Function to generate directions based on a list of points
 function generateDirections(p1,p2,p3) {
-
     const v1 = vectorFromPoints(p1, p2);
     const v2 = vectorFromPoints(p2, p3);
-
     const cross = crossProduct(v1, v2);
     if (cross < 0) {
         return "left";
@@ -17,9 +22,7 @@ function generateDirections(p1,p2,p3) {
     } else {
         return "";
     }
-
     
 }
-
 
 
